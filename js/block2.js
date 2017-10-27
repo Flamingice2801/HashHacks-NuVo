642d3f023eac06ca460c5de0769263213c4661e9
grufelous <grufelous@gmail.com>
2017-10-28 04:22:37 +0530

    Added more into the blockchain JS

--- js/block2.js
+++ js/block2.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256');
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
@@ -8,6 +9,16 @@ class Block {
     }
 
     calculateHash() {
-        
+        return SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString;
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "1509144683073", "gen", "0");
     }
 }
