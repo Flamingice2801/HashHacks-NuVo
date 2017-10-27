cef54a7307164708ebbecad0e7e5a64d169d713e
grufelous <grufelous@gmail.com>
2017-10-28 04:00:47 +0530

    Started with basic blockchain JS

---
+++ js/block2.js
@@ -0,0 +1,13 @@
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = calculateHash();
+    }
+
+    calculateHash() {
+        
+    }
+}
