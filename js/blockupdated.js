6870f81c8b854103f672663f15d77d606bf2f405
grufelous <grufelous@gmail.com>
2017-10-28 08:33:39 +0530

    Blocks created

--- js/blockupdated.js
+++ js/blockupdated.js
@@ -36,26 +36,44 @@ require.config({
 
 require(["crypto-js/aes", "crypto-js/sha256"], function (AES, SHA256) {
     class Block {
+        calculateHash() {
+            var hashed = (SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString() + "");
+            alert(hashed);
+            return hashed;
+        }
         constructor(index, timestamp, data, previousHash = '') {
             this.index = index;
             this.timestamp = timestamp;
             this.data = data;
             this.previousHash = previousHash;
-            this.hash = calculateHash();
-        }
-
-        calculateHash() {
-            return SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString;
-        }
+            this.hash = this.calculateHash();
+        }        
     }
 
     class Blockchain {
         constructor() {
             this.chain = [];
+            this.createGenesisBlock();
         }
 
         createGenesisBlock() {
             return new Block(0, "1509144683073", "gen", "0");
         }
+
+        getLatestBlock() {
+            return this.chain[this.chain.length-1];
+        }
+
+        /*addBlock(newBlock) {
+            newBlock.previousHash = this.getLatestBlock.hash;
+            newBlock.hash = newBlock.calculateHash();
+            this.chain.push(this.newBlock);
+        }*/
     }
+
+    let b = new Blockchain();
+    b.addBlock(new Block(1, Date.now().toString, {candidate: 1}));
+    b.addBlock(new Block(2, Date.now().toString, {candidate: 3}));
+
+    console.log(JSON.stringify(b, null, 4));
 });
