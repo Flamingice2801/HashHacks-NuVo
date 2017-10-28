4c5cf57e2afd06404ffd43d0e0eeafc009a55dda
Amol <29180487+frextrite@users.noreply.github.com>
2017-10-28 06:38:18 +0530

    Fixed blockupdated.js to use nodejs

--- js/blockupdated.js
+++ js/blockupdated.js
@@ -1,4 +1,4 @@
-const SHA256 = require('crypto-js/sha256');
+/*const SHA256 = require('crypto-js/sha256');
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
@@ -21,4 +21,41 @@ class Blockchain {
     createGenesisBlock() {
         return new Block(0, "1509144683073", "gen", "0");
     }
-}
+}*/
+
+
+require.config({
+    packages: [
+        {
+            name: 'crypto-js',
+            location: 'crypto-js',
+            main: 'index'
+        }
+    ]
+});
+
+require(["crypto-js/aes", "crypto-js/sha256"], function (AES, SHA256) {
+    class Block {
+        constructor(index, timestamp, data, previousHash = '') {
+            this.index = index;
+            this.timestamp = timestamp;
+            this.data = data;
+            this.previousHash = previousHash;
+            this.hash = calculateHash();
+        }
+
+        calculateHash() {
+            return SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString;
+        }
+    }
+
+    class Blockchain {
+        constructor() {
+            this.chain = [];
+        }
+
+        createGenesisBlock() {
+            return new Block(0, "1509144683073", "gen", "0");
+        }
+    }
+});
